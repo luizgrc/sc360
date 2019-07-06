749261c8180e5d4abccf913e6191e3b1ef0c7e12
Luis García <luisgrc11312538@gmail.com>
2019-07-05 20:10:19 -0500

    se agrega actualizacion base de datos seyci , traslado

---
+++ BD/ADD columnnumfila.sql
@@ -0,0 +1,20 @@
+﻿select EXP from seyci
+
+select numfila from tipoarchivo where idTipoArchivo=1
+show SP_LISTAR_SOLICITUDES
+
+
+ALTER TABLE tipoarchivo
+  ADD numfila int   --NOT NULL O NULL?
+  AFTER fechaRegistro;
+
+update tipoarchivo set numfila =9 where idTipoArchivo=1; --  'ARCHIVO DE SOLICITUDES'
+update tipoarchivo set numfila =2 where idTipoArchivo=2; --  'ARCHIVO DE BENEFICIARIOS'
+update tipoarchivo set numfila =13 where idTipoArchivo=3; -- 'ARCHIVO DE SEYCI'
+update tipoarchivo set numfila =0 where idTipoArchivo=4; --  'ARCHIVO DE CARGA DE NOMINA'
+
+update tipoarchivo set numfila =0 where idTipoArchivo=5; --   'ARCHIVO DE CARGA DE BONOS REDIMIDOS'
+update tipoarchivo set numfila =0 where idTipoArchivo=6; --   'ARCHIVO DE CARGA DE DICTAMENES COMAFP Y COMEC'
+update tipoarchivo set numfila =0 where idTipoArchivo=7; --   'ARCHIVO DE CARGA DE APORTE ADICIONAL' 
+update tipoarchivo set numfila =0 where idTipoArchivo=8; --   'ARCHIVO DE CARGA DE PRIMER PAGO'
+update tipoarchivo set numfila =0 where idTipoArchivo=9; --   'ARCHIVO DE RETIRO 95.5 - 25% / DECRETO DE URGENCIA(DU)' 
---
+++ BD/Changelog.sql
@@ -0,0 +1,66 @@
+﻿/*..........-------------CHANGELOG 1.1----------------........ */
+alter table seyci add column MODIFICADO_POR varchar(50);
+alter table seyci add column MODIFICADO_FECHA varchar(50);
+alter table TRASLADOSEYCI add column MODIFICADO_POR varchar(50);
+alter table TRASLADOSEYCI add column MODIFICADO_FECHA varchar(50);
+--Cambios en SP_LISTAR_DETALLE_SOLICITUD  se quito EXP
+--Cambios en SP_ACTUALIZAR_SEYCI_DG aumendo de MODIFICADO_POR y MODIFICADO_FECHA
+--Cambios en SP_ACTUALIZAR_SEYCI_TAB aumendo de MODIFICADO_POR y MODIFICADO_FECHA
+--Cambios en SP_LISTAR_TRASLADO_SEYCI
+--Cambios en SP_INSERT_UPDATE_TRASLADO aumendo de MODIFICADO_POR y MODIFICADO_FECHA 
+/*..........-------------CHANGELOG 1.1----------------........ */
+SELECT * FROM TRASLADOSEYCI
+
+/*..........-------------CONSULTAS----------------........ */
+/*TAB 1*/
+--SP_LISTAR_DETALLE_SOLICITUD 
+call SP_LISTAR_DETALLE_SOLICITUD('107030')
+select * from seyci
+/*TAB 2*/
+--SP_LISTAR_DETALLE_SEYCI
+call SP_LISTAR_DETALLE_SEYCI('107030')
+select * from seyci
+/*TAB 3*/
+--SP_LISTAR_TRASLADO_SEYCI
+select * from TRASLADOSEYCI
+
+/*TAB 4*/
+--SP_LISTAR_REEVALUACION
+select * from REEVALUACION
+/*TAB 5*/
+--SP_LISTAR_DICTAMEN
+select * from SEYCI
+/*TAB 6*/
+--SP_LISTAR_APELACION
+/*..........-------------CONSULTAS----------------........ */
+
+
+/*..........-------------UPDATE----------------........ */
+/*TAB 1*/
+--SP_ACTUALIZAR_SEYCI_DG
+
+
+/*TAB 2*/
+--SP_ACTUALIZAR_SEYCI_TAB
+/*TAB 3*/
+--SP_INSERT_UPDATE_TRASLADO
+/*TAB 4*/
+--SP_INSERT_UPDATE_REEVALUACION
+
+/*TAB 5*/
+/*TAB 6*/
+
+
+
+
+/*..........-------------UPDATE----------------........ */
+select * from seyci
+describe usuario
+select NUMEROSOLICITUD , count(NUMEROSOLICITUD) from SEYCI 
+group by NUMEROSOLICITUD
+having count(NUMEROSOLICITUD) >1
+describe reevaluacion
+
+
+
+
